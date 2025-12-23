edbb4b83db4acb61db639a3d260ead04e4b6cae3
Justin-K-Ellis <jlke45@gmail.com>
2025-12-23 22:00:49 +0900

    feat: setup basic boilerplate for supplying text
    English only to start.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from "next-intl";
+
 export default function Navbar() {
+  const t = useTranslations("Navbar");
+
   return (
     <nav className="flex justify-between items-center shadow p-1 md:p-2">
-      <h1 className="font-bold text-3xl md:text-4xl">Eiki</h1>
+      <h1 className="font-bold text-3xl md:text-4xl">{t("title")}</h1>
       <ul className="flex gap-1 md:gap-2">
-        <li>Login</li>
-        <li>About</li>
+        <li>{t("login")}</li>
+        <li>{t("about")}</li>
         <li>EN</li>
       </ul>
     </nav>
