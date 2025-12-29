04346309265ca5d8aaa0328948a726ea6c1730ff
Justin-K-Ellis <jlke45@gmail.com>
2025-12-29 18:36:36 +0900

    chore: delete dead code

--- src/db/seeding/seedMain.ts
+++ src/db/seeding/seedMain.ts
@@ -1,11 +1,5 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { unitsTable } from "../schema";
-import type { UnitDTO } from "../schema";
-
 import seedUnits from "./seedUnits";
-
-const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
   await seedUnits();
