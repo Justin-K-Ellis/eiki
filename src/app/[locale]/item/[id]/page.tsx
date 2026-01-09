d6b3678a128d9dcad6e03f33440c6668b884a326
Justin-K-Ellis <jlke45@gmail.com>
2026-01-09 13:17:29 +0900

    feat: add localization for item cards

--- src/app/[locale]/item/[id]/page.tsx
+++ src/app/[locale]/item/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { getTranslations } from "next-intl/server";
 import itemService from "@/services/Items.service";
 import ItemCard from "@/components/ItemCard";
 
@@ -8,7 +9,7 @@ export default async function AnItem({
 }) {
   const { id } = await params;
   const item = await itemService.getItem(parseInt(id));
-  console.log(item);
+  const t = await getTranslations("ItemCard");
 
   return (
     <div>
@@ -17,6 +18,13 @@ export default async function AnItem({
         body={item.passage.body}
         options={item.options}
         japaneseTranslation={item.passage.ja_translation}
+        promptLabel={t("questionPrompt")}
+        explanationLabel={t("explanation")}
+        ansBtnLabel={t("answerBtn")}
+        isAnsLabel={t("isAnswer")}
+        enPassLabel={t("enPassageMarker")}
+        jaPassLabel={t("jaPassageMarker")}
+        backBtnLabel={t("backBtn")}
       />
     </div>
   );
