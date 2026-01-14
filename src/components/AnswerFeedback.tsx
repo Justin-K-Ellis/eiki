5d99bd586dd65d80d874d7fba2200e71f0929ddd
Justin-K-Ellis <jlke45@gmail.com>
2026-01-14 22:00:29 +0900

    feat: internationalize AnswerFeedback component

--- src/components/AnswerFeedback.tsx
+++ src/components/AnswerFeedback.tsx
@@ -5,24 +5,25 @@ import {
   ItemTitle,
 } from "@/components/ui/item";
 import { BadgeCheckIcon, X } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 interface AnswerFeedbackProps {
   correct: boolean;
 }
 
 export default function AnswerFeedback({ correct }: AnswerFeedbackProps) {
-  const message = correct ? `Correct!` : `Incorrect`;
-  const color = correct ? "text-green-500" : "text-red-500";
+  const t = useTranslations("AnswerFeedback");
+
+  const message = correct ? t("correctMsg") : t("incorrectMsg");
+  const color = correct ? "text-green-600" : "text-red-500";
 
   return (
     <Item variant="outline">
       <ItemContent>
-        <ItemTitle>Answer</ItemTitle>
-        <ItemDescription className={`${color} flex gap-2`}>
-          {/* <p className={color}> */}
+        <ItemTitle>{t("answerTag")}</ItemTitle>
+        <ItemDescription className={`${color} flex gap-2 items-center`}>
           {correct ? <BadgeCheckIcon /> : <X />}
           {message}
-          {/* </p> */}
         </ItemDescription>
       </ItemContent>
     </Item>
