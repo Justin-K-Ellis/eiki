9862936bbd5959c6891b826df04c8eadb6508986
Justin-K-Ellis <jlke45@gmail.com>
2026-03-03 11:34:39 +0900

    feat: extract home page content into HomePageCard component

--- src/app/[locale]/vocab/page.tsx
+++ src/app/[locale]/vocab/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Vocab() {
   return <div>Coming soon.</div>;
 }
--- src/components/HomePageCard.tsx
+++ src/components/HomePageCard.tsx
@@ -19,18 +19,16 @@ interface HPCardProps {
 export default function HomePageCard(props: HPCardProps) {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{props.title}</CardTitle>
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl">{props.title}</CardTitle>
         <CardDescription>{props.description}</CardDescription>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col gap-2 justify-center items-center">
           <p>{props.content}</p>
-          <CardAction>
-            <Link href={props.link}>
-              <Button>Go</Button>
-            </Link>
-          </CardAction>
+          <Link href={props.link} className="w-1/4">
+            <Button className="w-full">Go</Button>
+          </Link>
         </div>
       </CardContent>
     </Card>
