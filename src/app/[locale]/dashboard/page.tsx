6c25bffb5559904b1f2a3c90ba74a39277edfcd2
Justin-K-Ellis <jlke45@gmail.com>
2026-03-07 21:45:57 +0900

    feat: create dashboard to show user progress

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -8,61 +8,81 @@ import {
 } from "@/components/ui/table";
 import { Progress } from "@/components/ui/progress";
 import { UserButton } from "@clerk/nextjs";
+import { getTranslations } from "next-intl/server";
 
-import { CEFRLevel } from "@/db/schema";
+import { CEFRLevel, cefrEnum } from "@/db/schema";
 import itemService from "@/services/Items.service";
 import usersService from "@/services/Users.service";
 import { currentUser } from "@clerk/nextjs/server";
 import getRoundedNum from "@/lib/getRoundedNum";
 
 export default async function Dashboard() {
-  const levels: CEFRLevel[] = ["A1", "A2", "B1", "B2", "C1", "C2"];
+  // User stuff
   const user = await currentUser();
   if (!user) {
     return <p>User not found!</p>;
   }
-  const name =
-    user.firstName ?? user.fullName ?? user.emailAddresses.toString();
+  const userName =
+    user.firstName ??
+    user.fullName ??
+    user.emailAddresses[0].emailAddress ??
+    "User";
 
-  const compStatsP = levels.map(async (level) => {
-    const totalPassageCount = await itemService.getNumOfPassagesByCEFR(level);
-    const completedCount = await usersService.getNumOfCompletedPassagesByCEFR(
-      user!.id,
-      level,
-    );
+  // internationalization stuff
+  const t = await getTranslations("Dashboard");
+  const titleMsg = t("title", { name: userName });
+  const manageAccountMsg = t("manageAccount");
+  const summaryTitleMsg = t("summaryTitle");
+  const levelDescMsg = t("level");
+  const completeMsg = t("complete");
+  const totalMsg = t("total");
+  const progressMsg = t("progress");
+
+  // level & completion data
+  const levels: CEFRLevel[] = cefrEnum.enumValues;
+
+  const [userCompStats, totalPassageCounts] = await Promise.all([
+    usersService.getCompletionStats(user.id),
+    itemService.getNumOfPassagesByCEFR(),
+  ]);
+
+  const compStats = levels.map((level) => {
+    const complete = userCompStats.get(level) ?? 0;
+    const total = totalPassageCounts.get(level) ?? 0;
     return {
       level: level,
-      complete: completedCount,
-      total: totalPassageCount,
+      complete: complete,
+      total: total,
+      percentDone: getRoundedNum(complete, total),
     };
   });
 
-  const compStats = await Promise.all(compStatsP);
-  const totalComplete = compStats.reduce((a, b) => a + b.complete, 0);
+  const totalComplete = compStats.reduce((acc, stat) => acc + stat.complete, 0);
 
   return (
     <div>
       <div id="account" className="flex justify-around">
-        <h1 className="text-3xl md:text-4xl font-bold">
-          {name}&apos; Dashboard
-        </h1>
+        <h1 className="text-2xl md:text-3xl font-bold">{titleMsg}</h1>
         <div className="flex flex-col md:flex-row gap-1 md:gap-2 items-center justify-center w-1/3">
-          <p className="text-sm text-zinc-600 text-center">Manage Account</p>
+          <p className="text-sm text-zinc-600 text-center">
+            {manageAccountMsg}
+          </p>
           <UserButton />
         </div>
       </div>
       <div className="shadow p-4 my-4">
         <p className="text-2xl md:text-3xl">
-          Total lessons completed: {totalComplete}
+          {summaryTitleMsg}
+          {totalComplete}
         </p>
       </div>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Level</TableHead>
-            <TableHead>Complete</TableHead>
-            <TableHead>Total</TableHead>
-            <TableHead className="text-right">Progress</TableHead>
+            <TableHead className="w-[100px]">{levelDescMsg}</TableHead>
+            <TableHead>{completeMsg}</TableHead>
+            <TableHead>{totalMsg}</TableHead>
+            <TableHead className="text-right">{progressMsg}</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -73,11 +93,9 @@ export default async function Dashboard() {
               <TableCell>{stats.total}</TableCell>
               <TableCell className="text-right">
                 <span>
-                  <Progress
-                    value={getRoundedNum(stats.complete, stats.total)}
-                  />
+                  <Progress value={stats.percentDone} />
                 </span>
-                {getRoundedNum(stats.complete, stats.total)}%
+                {stats.percentDone}%
               </TableCell>
             </TableRow>
           ))}
